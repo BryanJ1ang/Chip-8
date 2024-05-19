041df1841f5b6a121bbd8d3b754b3293c4c9d35b
Bryan <jbryan@student.ubc.ca>
2024-05-19 15:24:16 -0700

    Implemented memory

---
+++ include/memory.h
@@ -0,0 +1,23 @@
+#ifndef memory_h
+#define memory_h
+#include <cstdint>
+
+class Memory {
+    public:
+    
+        Memory(); 
+
+        uint8_t read(uint16_t address);
+
+        void write(uint16_t address, uint8_t data);
+
+        void initialize_font();
+
+    private:
+        uint8_t memory[4096];
+
+
+
+};
+
+#endif 
---
+++ src/memory.cpp
@@ -0,0 +1,57 @@
+#include <cstdint>
+#include "memory.h"
+#include <iostream>
+
+using std::cout, std::endl;
+
+    void Memory::initialize_font() {
+        uint8_t fonts[] = {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    };
+
+    int index = 0x50; // Starting index for the fonts in memory
+
+    for (int i = 0; i < sizeof(fonts); i++) {
+        write(index + i, fonts[i]); 
+    }
+
+    }
+
+    Memory::Memory() {
+        Memory::initialize_font();
+    }
+
+
+
+    uint8_t Memory::read(uint16_t address) {
+        if (address < 4095) {
+            return memory[address];
+        } else {
+            std::cout << "Read address out of Bounds" << std::endl;
+            return 0; // TODO: handle out of bounds
+        }
+    }
+
+    void Memory::write(uint16_t address, uint8_t data) {
+        if (address < 4095) {
+            memory[address] = data;
+        } else {
+            std::cout << "Write address out of Bounds" << std::endl;
+            // TODO: handle out of bounds
+        }
+    }
