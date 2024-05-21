0fad98a55ae247839d065e9924d11ec25902ba1e
Bryan <jbryan@student.ubc.ca>
2024-05-21 14:23:35 -0700

    Added memory index register for

--- include/registers.h
+++ include/registers.h
@@ -8,10 +8,13 @@ class Registers {
     public:
         uint8_t read(uint8_t address);
         void write(uint8_t address, uint8_t data);
+        void setIndex(uint16_t newIndex);
+        uint16_t Registers::getIndex();
 
 
     private:
         uint8_t registers[16];
+        uint16_t index;
 
 };
 
