552d77ff7066c3f1ca46e543141185c993be3121
Bryan <jbryan@student.ubc.ca>
2024-06-03 00:14:30 -0700

    Added delay and sound timer cycles

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,56 @@
 #include "cpu.h"
 #include "display.h"
-
+#include "timer.h"
 #include <vector>
 #include <string>
 #include <iostream>
 #include <chrono>
 #include <SDL.h>
 #include <thread>
-
+#include <chrono>
 
 using namespace std;
 
 #ifdef main
-#undef main
+#undef main 
 #endif 
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-
+std::string rom = "5-quirks.ch8";
 
 int main(int argc, char *argv[]) {
     std::cout << "Starting Chip 8 Emulator" << std::endl;
     CPU cpu;
 
-    if (!cpu.memory.loadProgram("D:\\vsC\\Chip-8\\chip8_programs\\IBM_Logo.ch8")) {
+    if (!cpu.memory.loadProgram("D:\\vsC\\Chip-8\\chip8_programs\\" + rom)) {
         std::cerr << "Error loading the CHIP-8 program.\n";
         return 0;
     }
-
     std::cout << "Program loaded" << std::endl;
+
     
     auto lastCycleTime = chrono::high_resolution_clock::now();
-    int cycleDelay = 16;  // milliseconds
+    int cycleDelay = 16;  // milliseconds between CPU cycles
+    float timerDelay = 1000 / 60;
+    auto lastTimerUpdate = chrono::high_resolution_clock::now();
 
-    while (cpu.display.isOpen()) { 
+    while (cpu.display.isOpen()) {     
         auto currentTime = chrono::high_resolution_clock::now();
-        float dt = chrono::duration<float, milli>(currentTime - lastCycleTime).count();
+        auto dt = chrono::duration<float, milli>(currentTime - lastCycleTime).count();
+        auto dtTimer = chrono::duration<float, milli>(currentTime - lastTimerUpdate).count();
+
+        if (dtTimer > timerDelay) {
+            lastTimerUpdate = currentTime;
+            if (cpu.timer.delay_timer > 0) cpu.timer.delay_timer -= 1;
+            if (cpu.timer.sound_timer > 0) {
+                cpu.timer.sound_timer -= 1; // insert play beep
+            }
+        }
 
         if (dt > cycleDelay) {
             lastCycleTime = currentTime;
 
-            cout << "CPU Cycle" << endl;
             cpu.fetch();
             cpu.decodeExecute();
             cpu.display.update();
