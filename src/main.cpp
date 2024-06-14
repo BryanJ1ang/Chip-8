78e08b8fcb442f2e8c9cd724387ceadc3fe4a9ee
Bryan <jbryan@student.ubc.ca>
2024-06-14 01:51:05 -0700

    Added polling for key inputs in main function

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <vector>
 #include <string>
 #include <iostream>
-#include <chrono>
 #include <SDL.h>
 #include <thread>
 #include <chrono>
@@ -17,7 +16,7 @@ using namespace std;
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-std::string rom = "5-quirks.ch8";
+std::string rom = "flightrunner.ch8";
 
 int main(int argc, char *argv[]) {
     std::cout << "Starting Chip 8 Emulator" << std::endl;
@@ -31,8 +30,8 @@ int main(int argc, char *argv[]) {
 
     
     auto lastCycleTime = chrono::high_resolution_clock::now();
-    int cycleDelay = 16;  // milliseconds between CPU cycles
-    float timerDelay = 1000 / 60;
+    int cycleDelay = 1;  // milliseconds between CPU cycles
+    float timerDelay = 1000.0f / 60;
     auto lastTimerUpdate = chrono::high_resolution_clock::now();
 
     while (cpu.display.isOpen()) {     
@@ -41,28 +40,31 @@ int main(int argc, char *argv[]) {
         auto dtTimer = chrono::duration<float, milli>(currentTime - lastTimerUpdate).count();
 
         if (dtTimer > timerDelay) {
-            lastTimerUpdate = currentTime;
-            if (cpu.timer.delay_timer > 0) cpu.timer.delay_timer -= 1;
+            lastTimerUpdate = currentTime;  // to avoid time drift
+            if (cpu.timer.delay_timer > 0) {
+                cpu.timer.delay_timer -= 1;
+            }
             if (cpu.timer.sound_timer > 0) {
                 cpu.timer.sound_timer -= 1; // insert play beep
             }
         }
+            SDL_Event event;
+            while (SDL_PollEvent(&event)) {
+                        if (event.type == SDL_KEYUP) {
+                            cpu.key = 0xff;
+                        } else if (event.type == SDL_KEYDOWN) {
+                            cpu.key = cpu.keyTranslate(event.key.keysym.sym);
+                        } else if (event.type == SDL_QUIT) {
+                            cpu.display.setClosed();
+                        }
+                    }
 
         if (dt > cycleDelay) {
             lastCycleTime = currentTime;
-
+                    
             cpu.fetch();
             cpu.decodeExecute();
             cpu.display.update();
-        
-            SDL_Event event;
-            while (SDL_PollEvent(&event)) {
-                if (event.type == SDL_QUIT) {
-                    cpu.display.setClosed();
-                }
-            }
         }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
